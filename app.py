126aa3d6a3469fc93db8b1248a762edde5146a1a
Kiki2356 <49116646+Kiki2356@users.noreply.github.com>
2023-06-13 03:43:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,67 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import sqlite3
-import os
-
-host = os.getenv('IP', '0.0.0.0')
-port = int(os.getenv('PORT', 5000))
-
+import subprocess
 
 app = Flask(__name__)
-db_name = 'servers.db'
+DATABASE = 'database.db'
 
-def create_table():
-    conn = sqlite3.connect(db_name)
+def create_database():
+    conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS servers (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT)')
+    c.execute('''CREATE TABLE IF NOT EXISTS servers
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 url TEXT,
+                 status TEXT)''')
+    conn.commit()
+    conn.close()
+
+def add_server(url, status):
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute("INSERT INTO servers (url, status) VALUES (?, ?)", (url, status))
     conn.commit()
     conn.close()
 
 def get_servers():
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect(DATABASE)
     c = conn.cursor()
-    c.execute('SELECT * FROM servers')
+    c.execute("SELECT * FROM servers")
     servers = c.fetchall()
     conn.close()
     return servers
 
-def add_server(url):
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('INSERT INTO servers (url) VALUES (?)', (url,))
-    conn.commit()
-    conn.close()
-
-def remove_server(server_id):
-    conn = sqlite3.connect(db_name)
-    c = conn.cursor()
-    c.execute('DELETE FROM servers WHERE id = ?', (server_id,))
-    conn.commit()
-    conn.close()
-
 @app.route('/')
 def index():
-    create_table()
-    servers = get_servers()
-    return render_template('index.html', servers=servers)
+    return render_template('index.html')
 
 @app.route('/check', methods=['POST'])
 def check():
     server_url = request.form['server_url']
-    
-    # Simulasi pemeriksaan server down
-    # Ubah logika ini sesuai dengan kebutuhan Anda
-    if server_url == 'http://example.com':
-        status = 'Server is down.'
-        add_server(server_url)
-    else:
+    try:
+        subprocess.check_output(["ping", "-c", "1", server_url])
         status = 'Server is running normally.'
+        add_server(server_url, 'Running')
+    except subprocess.CalledProcessError:
+        status = 'Server is down.'
+        add_server(server_url, 'Down')
 
+    return render_template('result.html', server_url=server_url, status=status)
+
+@app.route('/servers')
+def servers():
     servers = get_servers()
-    return render_template('result.html', server_url=server_url, status=status, servers=servers)
+    return render_template('servers.html', servers=servers)
 
-@app.route('/remove', methods=['POST'])
-def remove():
+@app.route('/delete', methods=['POST'])
+def delete():
     server_id = request.form['server_id']
-    remove_server(server_id)
-    servers = get_servers()
-    return render_template('index.html', servers=servers)
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute("DELETE FROM servers WHERE id=?", (server_id,))
+    conn.commit()
+    conn.close()
+    return redirect('/servers')
 
 if __name__ == '__main__':
-    app.run(host=host, port=port)
+    create_database()
+    app.run(debug=True)
